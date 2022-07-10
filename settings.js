9da78ee7af267591ac056c4d33ba71e2da57fff4
Pakaya67377374 <109017526+Pakaya67377374@users.noreply.github.com>
2022-07-10 15:43:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,11 +22,10 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
-
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.owner = ['+94752521338'] //Owner number in aive msg
+global.premium = ['+94752521338'] //Owner Number info
+global.ownernomer = '+94752521338' //Owner Number <<<
+global.ownername = ''//Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
